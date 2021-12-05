050e948e0d1e7a910750f52da2004056b393b8c0
Ravi <ravi@inflearn.com>
2021-12-05 14:32:36 +0900

    해설 영상 코드 따라해보기

--- src/index.js
+++ src/index.js
@@ -81,65 +81,91 @@ function getDisplayContent(inputs = []) {
   })}`;
 }
 
+const operatorFunctions = {
+  '+': (x, y) => x + y,
+  '-': (x, y) => x - y,
+  '*': (x, y) => x * y,
+  '/': (x, y) => x / y,
+};
+
+function calculate(operator, accumulator, number) {
+  return operatorFunctions[operator]?.(accumulator, number) || number;
+}
+
 /**
  * @param {{ inputs: Array<string | number> }} props
  */
-function render({ inputs = [] } = { inputs: [] }) {
-  function handleClickNumber(number) {
-    if (typeof getLastInput(inputs) === 'number') {
-      const newInputs = [...inputs];
-      newInputs[getLastIndex(inputs)] = getLastInput(inputs) * 10 + number;
-      render({ inputs: newInputs });
-      return;
-    }
-    render({ inputs: [...inputs, number] });
+function render({ accumulator, number, operator }) {
+  function handleClickNumber(value) {
+    render({
+      accumulator,
+      number: (number || 0) * 10 + value,
+      operator,
+    });
   }
 
-  function handleClickOperator(operator) {
-    if (inputs.length === 0) {
-      return;
-    }
-    const lastInput = getLastInput(inputs);
-    if (typeof lastInput !== 'number') {
-      const newInputs = [...inputs];
-      newInputs[getLastIndex(inputs)] = operator;
-      render({ inputs: newInputs });
-      return;
-    }
-    render({ inputs: [...inputs, operator] });
+  function handleClickOperator(value) {
+    return render({
+      accumulator: calculate(operator, accumulator, number),
+      number: null,
+      operator: value,
+    });
   }
+  // function handleClickNumber(number) {
+  //   if (typeof getLastInput(inputs) === 'number') {
+  //     const newInputs = [...inputs];
+  //     newInputs[getLastIndex(inputs)] = getLastInput(inputs) * 10 + number;
+  //     render({ inputs: newInputs });
+  //     return;
+  //   }
+  //   render({ inputs: [...inputs, number] });
+  // }
+
+  // function handleClickOperator(operator) {
+  //   if (inputs.length === 0) {
+  //     return;
+  //   }
+  //   const lastInput = getLastInput(inputs);
+  //   if (typeof lastInput !== 'number') {
+  //     const newInputs = [...inputs];
+  //     newInputs[getLastIndex(inputs)] = operator;
+  //     render({ inputs: newInputs });
+  //     return;
+  //   }
+  //   render({ inputs: [...inputs, operator] });
+  // }
 
   const element = (
     <section className="calculator">
       <h1>간단 계산기</h1>
       <div className="calculator__display">
-        <p>{getDisplayContent(inputs)}</p>
+        <p>{number || accumulator || 0}</p>
       </div>
       <ul className="calculator__numbers">
         {[...Array(9).keys()].map((_, index) => {
-          const number = (index + 1) % 10;
+          const currentNumber = (index + 1) % 10;
           return (
-            <li data-key={number}>
+            <li data-key={currentNumber}>
               <button
                 type="button"
-                aria-label={number}
-                onClick={() => handleClickNumber(number)}
+                aria-label={currentNumber}
+                onClick={() => handleClickNumber(currentNumber)}
               >
-                {number}
+                {currentNumber}
               </button>
             </li>
           );
         })}
       </ul>
       <ul className="calculator__operators">
-        {operators.map((operator) => (
-          <li data-key={operator.name}>
+        {operators.map((currentOperator) => (
+          <li data-key={currentOperator.name}>
             <button
               type="button"
-              aria-label={operator.name}
-              onClick={() => handleClickOperator(operator)}
+              aria-label={currentOperator.name}
+              onClick={() => handleClickOperator(currentOperator.symbol)}
             >
-              {operator.symbol}
+              {currentOperator.symbol}
             </button>
           </li>
         ))}
@@ -152,4 +178,8 @@ function render({ inputs = [] } = { inputs: [] }) {
   app.appendChild(element);
 }
 
-render();
+render({
+  accumulator: null,
+  number: null,
+  operator: null,
+});
